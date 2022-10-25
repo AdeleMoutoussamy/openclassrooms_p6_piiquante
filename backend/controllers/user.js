8e79efbda23836599281e5403ffaf9ee4f6ac278
Adele M <moutoussamy.adele@gmail.com>
2022-10-25 19:09:08 +0400

    sauce.js controllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 // package qui permet de créer et vérifier les tokens d'authentification
 const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
+const User = require('../models/user');
 
 // J'exporte une fonction pour l'enregistrement d'un utilisateur
 exports.signup = (req, res, next) =>
@@ -19,7 +19,7 @@ exports.signup = (req, res, next) =>
         });
         // Enregistre l'objet dans la base de données
         user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .then(() => res.status(201).json({ message: " Utilisateur créé ! " }))
         .catch(error => res.status(400).json({ error }));
     })
     .catch(error =>
@@ -38,7 +38,7 @@ exports.login = (req, res, next) =>
     {
         if (!user)
         {
-            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            return res.status(401).json({ error: " Utilisateur non trouvé ! " });
         }
         // Méthode compare vérifie si un mot de passe entré par l'utilisateur correspond à un hash sécurisé enregistré en base de données
         bcrypt.compare(req.body.password, user.password)
@@ -46,7 +46,7 @@ exports.login = (req, res, next) =>
         {
             if (!valid)
             {
-                return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                return res.status(401).json({ error: " Mot de passe incorrect ! " });
             }
             res.status(200).json({
                 userId: user._id,
