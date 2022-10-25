1cdb5ad7455223249895f7228f93420e9c790daf
Adele M <moutoussamy.adele@gmail.com>
2022-10-25 19:08:10 +0400

    sauce.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,16 @@
 // J'importe mongoose
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose');
+// Permet de ne pas avoir plusieurs comptes avec le même email
+const uniqueValidator = require('mongoose-unique-validator');
 
 // Schéma de données
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 });
 
 // Je l'applique au schéma
 userSchema.plugin(uniqueValidator);
 
 // J'exporte le modèle terminé
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
